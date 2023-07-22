01776b544ec1d375f181b9684429df4f31aa04a0
TaDucChi <21012396@st.phenikaa-uni.edu.vn>
2023-07-22 23:18:15 +0700

    commit

--- index.js
+++ index.js
@@ -32,7 +32,28 @@ document.body.onload = function () {
 
     var exitmenu = document.querySelectorAll(".exit__menu")[0];
    
+
+    $(".exit__menu").click(function () {
+        $(".mb__navbar").toggle();
+    });
+
+    $(".menu__navbar").click(function () {
+        $(".mb__navbar").toggle();
+    })
+
+    $(".mb__navbar>div>ul>li").click(function () {
+        $(".mb__navbar").hide();
+    })
+    var header = document.getElementsByClassName("header")[0];
+    header.style["background-color"] = "#EA906C";
+
+
+   
 }
+
+
+
+
 
 
 
