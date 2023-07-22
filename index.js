6a6f3b127b68de24863222d85ebac065585cc0dc
TaDucChi <21012396@st.phenikaa-uni.edu.vn>
2023-07-22 22:10:16 +0700

    Commit

--- index.js
+++ index.js
@@ -36,23 +36,3 @@ document.body.onload = function () {
 
 
 
-document.body.onload = function () {
-
-
-    $(".exit__menu").click(function () {
-        $(".mb__navbar").toggle();
-    });
-
-    $(".menu__navbar").click(function () {
-        $(".mb__navbar").toggle();
-    })
-
-    $(".mb__navbar>div>ul>li").click(function () {
-        $(".mb__navbar").hide();
-    })
-    var header = document.getElementsByClassName("header")[0];
-    header.style["background-color"] = "#EA906C";
-
-
-
-}
